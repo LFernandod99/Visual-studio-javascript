28f19e5afb417ccce9d708513dfe9dae4f91edb3
LFernandod99 <luizfernando.duartevidal1@gmail.com>
2022-09-08 16:07:58 -0300

    main

---
+++ exercicios.js
@@ -0,0 +1,42 @@
+/*let custo, percentual, valorI, valorF
+
+valorI = prompt("insira o valor pago: ");
+percentual = prompt("Insira o percentual adicionado para revenda: ");
+
+custo = (percentual /100) * valorI
+valorF = parseInt(custo) + parseInt(valorI)
+
+alert("preço de custo é sobre esse produto é: " + custo + "\nValor total é: "+ valorF);*/
+
+/*let valorD, valorTotal
+
+valorD = prompt("Digite o valor depositado: ");
+
+ valorTotal = 0.007 * parseInt(valorD) + parseInt(valorD)
+//valorTotal = 0.007 * (+valorD) + (+valorD)
+
+alert("O valor total em sua poupança é: " + valorTotal)*/
+
+/*let real, dolar, cotacao
+
+dolar = prompt("Digite o valor em dolar que deseja ser convertido: ");
+cotacao = prompt("Qual a cotação do dolar? ");
+real = dolar * cotacao
+
+alert("Você digitou U$ " + dolar + "\nOvalor da cotação é R$ " + cotacao + "\nA conversão para real é R$ " + real)*/
+
+let numero
+
+numero = prompt("Digite um número: ")
+
+if (numero > 80) {
+
+    alert("Este numero é maior que 80")
+    
+}else if (numero < 25) {
+    alert("Esse número é menor que 25")
+    
+}else if(numero == 40) {
+    alert("Você esclheu o 40")
+    
+}
